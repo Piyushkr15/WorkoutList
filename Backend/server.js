901c5419d800787fbc366abf1c9806738ff2da17
Piyush Kumar Anand <piyush@PIYUSHs-MacBook-Pro.local>
2023-10-07 19:11:01 +0530

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const path = require("path")
 
 //express app
 const app = express()
@@ -20,6 +21,24 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
+
+
+// --------------------------deployment------------------------------
+// __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) =>
+        // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
+// --------------------------deployment------------------------------
+
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
